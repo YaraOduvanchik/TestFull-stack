4626cec8302bf5bcc93bd3734a9b42d0b5cce329
Nikolay <Cherashev4@gmail.com>
2025-03-21 19:01:03 +0500

    fix

---
+++ Backend/Backend/Migrations/20250321133435_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "client",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClientName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "data_item",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<int>(type: "integer", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_data_item", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "dates",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false),
+                    dt = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dates", x => new { x.id, x.dt });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "client_contacts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClientId = table.Column<long>(type: "bigint", nullable: false),
+                    ContactType = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    ContactValue = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client_contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_client_contacts_client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_client_contacts_ClientId",
+                table: "client_contacts",
+                column: "ClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "client_contacts");
+
+            migrationBuilder.DropTable(
+                name: "data_item");
+
+            migrationBuilder.DropTable(
+                name: "dates");
+
+            migrationBuilder.DropTable(
+                name: "client");
+        }
+    }
+}
